5455abb0f83a0f02e86ee775c94c8078eb211f7e
Dilshodjon <torex.amaki@gmail.com>
2023-09-20 19:30:52 +0500

    first

---
+++ lib/elements/text_filds.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TextFildWidget extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+
+  const TextFildWidget({super.key,
+    required this.controller,
+    required this.labelText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
+    return Container(
+      height: h * 0.06,
+      width: w * 0.9,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(7),
+        color: Colors.grey[200],
+      ),
+      child: TextField(
+        keyboardType: TextInputType.name,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          hintText: labelText,
+          hintStyle: const TextStyle(color: Colors.black54),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(7)),
+            borderSide: BorderSide.none,
+          ),
+        ),
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: w * 0.04 > 20 ? 20 : w * 0.04,
+        ),
+      ),
+    );
+  }
+}
+
--- lib/pages/users_data.dart
+++ lib/pages/users_data.dart
@@ -1,21 +1,126 @@
-
 import 'package:flutter/material.dart';
 
+import '../elements/text_filds.dart';
+
 class LoginUserData extends StatelessWidget {
-  const LoginUserData({super.key});
+  LoginUserData({super.key});
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
+  final TextEditingController nikNameController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+
+  //var caun =
+  var countries = ['Uzbekistan', 'Russia', 'USA', 'China', 'Korea'];
+  var dropdownValue = 'Uzbekistan';
 
   @override
   Widget build(BuildContext context) {
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('LoginUserData'),
-          ],
-        ),
+      //resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      //AssetImage('assets/images/user.png') vertical center
+      body: SingleChildScrollView(
+        child: SizedBox(
+            width: w,
+            child: Column(
+              children: [
+                SizedBox(height: h * 0.1),
+                const Image(image: AssetImage('assets/images/logo.png')),
+                SizedBox(height: h * 0.01),
+                const Text(
+                  'Ilovadan foydalanish uchun ma’lumotlarni toldiring',
+                ),
+                SizedBox(height: h * 0.01),
+                Container(
+                  height: h * 0.15,
+                  width: w * 0.3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  //circular image radius 10
+                  child: Image.asset('assets/images/user.png'),
+                ),
+                Text(
+                  'Upload image',
+                  style: TextStyle(
+                    fontSize: w * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: h * 0.02),
+                TextFildWidget(
+                  controller: nameController,
+                  labelText: 'Ism',
+                ),
+                SizedBox(height: h * 0.02),
+                TextFildWidget(
+                  controller: surnameController,
+                  labelText: 'Familiya',
+                ),
+                SizedBox(height: h * 0.02),
+                TextFildWidget(
+                  controller: nikNameController,
+                  labelText: 'Nikname',
+                ),
+                SizedBox(height: h * 0.02),
+                TextFildWidget(
+                  controller: phoneNumberController,
+                  labelText: 'Telefon raqam',
+                ),
+                //dropdown
+                SizedBox(height: h * 0.02),
+                Container(
+                  height: h * 0.06,
+                  width: w * 0.9,
+                  padding: EdgeInsets.only(left: w * 0.02),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                      value: dropdownValue,
+                      onChanged: (String? newValue) {
+                        dropdownValue = newValue!;
+                      },
+                      items: countries.map<DropdownMenuItem<String>>(
+                        (String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: h * 0.02),
+                ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.transparent,
+                    backgroundColor: Colors.blue,
+                    minimumSize: Size(w * 0.9, h * 0.06),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Saqlash',
+                    style: TextStyle(
+                      fontSize: w * 0.04 > 20 ? 20 : w * 0.04,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            )),
       ),
     );
   }
