bd49d95e69fe6ae7dded8be6899d4f4824366d30
Dilshodjon <torex.amaki@gmail.com>
2023-09-27 19:18:16 +0500

    first

--- lib/api/api_controller.dart
+++ lib/api/api_controller.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
+import 'package:time_up/models/last_send_sms.dart';
 
 import '../models/send_sms.dart';
 
@@ -12,23 +14,29 @@ class ApiController extends GetxController {
   //{{host}}/api/v1/sms/last-sent-sms
   var lastSmsUrl = 'sms/last-sent-sms';
 
-  Future<int> sendSms(String phoneNumber) async {
+  Future<String> sendSms(String phoneNumber) async {
     var response = await http.post(
       Uri.parse(url + smsUrl),
       body: {
         "phone_number": phoneNumber,
       },
     );
-    print(response.body);
-    return 100;
+    if (response.statusCode == 200) {
+      return getLastSms(phoneNumber).then((value) => value.res?.code ?? '');
+    } else {
+      return '';
+    }
   }
 
-  Future<SendSms> getLastSms() async {
-    var response = await http.get(
+  Future<LastSendSms> getLastSms(String phoneNumber) async {
+    var response = await http.post(
       Uri.parse(url + lastSmsUrl),
+      body: {
+        "phone_number": phoneNumber,
+      },
     );
     print(response.body);
-    return sendSmsFromJson(response.body);
+    return LastSendSms.fromJson(jsonDecode(response.body));
   }
 
 
