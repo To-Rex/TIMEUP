84956327c3d921067e95297996ec0a5d04e7a41c
Dilshodjon <torex.amaki@gmail.com>
2023-10-01 09:47:45 +0500

    first

--- lib/pages/users_data.dart
+++ lib/pages/users_data.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:time_up/api/api_controller.dart';
