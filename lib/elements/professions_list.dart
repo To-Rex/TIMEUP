d14f41fa13255dca477e7a491e3c3e092d51689e
to-rex <torex.amaki@gmail.com>
2023-12-25 02:59:55 +0500

    first

--- lib/elements/professions_list.dart
+++ lib/elements/professions_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:time_up/api/api_controller.dart';
 
@@ -33,7 +34,7 @@ class ProfessionsList extends StatelessWidget {
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
     ApiController().getCategory();
-    return Column(
+    /*return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -138,6 +139,204 @@ class ProfessionsList extends StatelessWidget {
                   )
                 : const Center(child: Text('No data')))),
       ],
+    );*/
+    return Expanded(
+      child: Container(
+        color: Colors.white,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              top: 0,
+              child: Container(
+                  height: h * 0.2,
+                  width: w,
+                  margin: EdgeInsets.only(bottom: h * 0.02),
+                  color: Colors.blue,
+                  child: Column(
+                    children: [
+                      SizedBox(height: h * 0.03),
+                      Text(
+                        'Kasblar royhati',
+                        style: TextStyle(
+                          fontSize: w * 0.05,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+            Positioned(
+              top: h * 0.15,
+              child: Container(
+                  height: h * 0.1,
+                  width: w * 0.9,
+                  margin: EdgeInsets.only(left: w * 0.05, right: w * 0.05),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(width: w * 0.04),
+                      HeroIcon(
+                        HeroIcons.userCircle,
+                        color: Colors.orangeAccent,
+                        size: w * 0.1,
+                      ),
+                      SizedBox(width: w * 0.04),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Admin bilan aloqa',
+                            style: TextStyle(
+                              fontSize: w * 0.045,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          Text(
+                            'Taklif va shukoyatlar uchun',
+                            style: TextStyle(
+                              fontSize: w * 0.03,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Expanded(child: SizedBox()),
+                      Container(
+                        height: h * 0.04,
+                        width: w * 0.08,
+                        margin: EdgeInsets.only(right: w * 0.04),
+                        decoration: BoxDecoration(
+                          color: Colors.orangeAccent,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        child: Center(
+                          child: HeroIcon(
+                            HeroIcons.phone,
+                            color: Colors.white,
+                            size: w * 0.05,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+              ),
+            ),
+            Positioned(
+              top: h * 0.15,
+              bottom: 0,
+              child: Container(
+                width: w,
+                margin: EdgeInsets.only(top: h * 0.15),
+                color: Colors.white,
+                child: Obx(() => _getController.category.value.res == null
+                    ? const Center(child: Text('No data'))
+                    : Obx(() => _getController.category.value.res!.isNotEmpty
+                    ? SizedBox(
+                    width: w,
+                    child: SmartRefresher(
+                      enablePullDown: true,
+                      enablePullUp: true,
+                      header: CustomHeader(
+                        builder: (BuildContext context, RefreshStatus? mode) {
+                          Widget body;
+                          if (mode == RefreshStatus.idle) {
+                            body = const Text("Ma`lumotlarni yangilash uchun tashlang");
+                          } else if (mode == RefreshStatus.refreshing) {
+                            body = const CircularProgressIndicator(
+                              color: Colors.blue,
+                              backgroundColor: Colors.white,
+                              strokeWidth: 2,
+                            );
+                          } else if (mode == RefreshStatus.failed) {
+                            body = const Text("Load Failed!Click retry!");
+                          } else if (mode == RefreshStatus.canRefresh) {
+                            body = const Text("Ma`lumotlarni yangilash uchun tashlang");
+                          } else {
+                            body = const Text("Ma`lumotlar yangilandi");
+                          }
+                          return SizedBox(
+                            height: h * 0.1,
+                            child: Center(child: body),
+                          );
+                        },
+                      ),
+                      footer: CustomFooter(
+                        builder: (BuildContext context, LoadStatus? mode) {
+                          Widget body;
+                          if (mode == LoadStatus.idle) {
+                            body = const SizedBox();
+                          } else if (mode == LoadStatus.loading) {
+                            body = const CircularProgressIndicator(
+                              color: Colors.blue,
+                              backgroundColor: Colors.white,
+                              strokeWidth: 2,
+                            );
+                          } else if (mode == LoadStatus.failed) {
+                            body = const Text("Load Failed!Click retry!");
+                          } else if (mode == LoadStatus.canLoading) {
+                            body = const SizedBox();
+                          } else {
+                            body = const Text("Ma`lumotlar yangilandi");
+                          }
+                          return SizedBox(
+                            height: h * 0.1,
+                            child: Center(child: body),
+                          );
+                        },
+                      ),
+                      controller: _refreshController,
+                      onRefresh: _onRefresh,
+                      onLoading: _onLoading,
+                      child: ListView.builder(
+                        itemCount: _getController.category.value.res?.length ?? 0,
+                        padding: EdgeInsets.only(left: w * 0.05, right: w * 0.05),
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              _getController.changeOccupation(_getController.category.value.res?[index].name ?? '');
+                              onTap(_getController.category.value.res?[index].id ?? 0);
+                            },
+                            child: Container(
+                              height: h * 0.06,
+                              margin: EdgeInsets.only(bottom: h * 0.02),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(w * 0.02),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  _getController.category.value.res?[index].name ?? '',
+                                  style: TextStyle(
+                                    fontSize: w * 0.04,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ))
+                    : const Center(child: Text('No data')))),
+              ),),
+          ],
+        ),
+      )
     );
   }
 }
--- lib/pages/sample_page.dart
+++ lib/pages/sample_page.dart
@@ -25,101 +25,111 @@ class SamplePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var w = MediaQuery.of(context).size.width;
     var h = MediaQuery.of(context).size.height;
-    return Scaffold(
+    return Obx(() => _getController.meUsers.value.status.toString().isEmpty
+        ? const Center(child: CircularProgressIndicator())
+        : Scaffold(
+      backgroundColor: _getController.index.value == 1
+          ? Colors.blue
+          : Colors.white,
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(h * 0.066),
           child: Obx(() => _getController.index.value != 5
               ? Container(
-                  height: h * 0.06,
-                  margin: EdgeInsets.only(top: h * 0.045),
-                  child: Row(
-                    children: [
-                      SizedBox(width: w * 0.04),
-                      Image(
-                        image: const AssetImage('assets/images/text.png'),
-                        width: w * 0.2,
-                        height: h * 0.05,
-                      ),
-                      Expanded(child: Container()),
-                      //notification icon
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => MessagingWidget()),
-                          );
-                        },
-                        child: HeroIcon(
-                          HeroIcons.bell,
-                          size: w * 0.07,
-                        ),
-                      ),
-
-                      SizedBox(width: w * 0.02),
-                    ],
-                  ),
-                )
-              : Container(height: h * 0.03))),
-      /*body: Obx(() => _getController.index.value == 3
-          ? SizedBox(
-              height: h * 0.9, child: _getController.widgetOptions.elementAt(_getController.index.value),
-            )
-          : SingleChildScrollView(
-              child: _getController.widgetOptions.elementAt(_getController.index.value),
-            )),*/
-      body: Obx(() => _getController.index.value == 3 || _getController.index.value == 4
-          ? SizedBox(
-            child: _getController.widgetOptions.elementAt(_getController.index.value),
-          ) : Column(
+            height: _getController.index.value == 1 ? h * 0.1 : h * 0.066,
+            color: _getController.index.value == 1
+                ? Colors.blue
+                : Colors.white,
+            margin: _getController.index.value == 1
+                ? const EdgeInsets.only(top: 0)
+                : EdgeInsets.only(top: h * 0.045),
+            padding: _getController.index.value == 1
+                ? EdgeInsets.only(top: h * 0.045)
+                : EdgeInsets.only(top: h * 0.01),
+            child: Row(
               children: [
-                Obx(() => _getController.uplAodVideo.value == true
-                    ? Container(
-                        width: w,
-                        height: h * 0.05,
-                        color: Colors.grey[300],
-                        padding: EdgeInsets.only(left: w * 0.02, right: w * 0.02),
-                        child: Row(
-                          children: [
-                            _getController.postFile.value != ''
-                                ? Container(
-                                    width: w * 0.1,
-                                    height: h * 0.04,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(3),
-                                      image: DecorationImage(
-                                        image: Image.file(File(_getController.postFile.value),).image,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  )
-                                : Container(),
-                            SizedBox(width: w * 0.02),
-                            Text('Yuklanmoqda...',
-                              style: TextStyle(
-                                fontSize: w * 0.04,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            Expanded(child: Container()),
-                            const SizedBox(
-                              width: 30,
-                              height: 30,
-                              child: CircularProgressIndicator(),
-                            )
-                          ],
-                        ))
-                    : const SizedBox()),
-                SizedBox(
-                  height: _getController.uplAodVideo.value == true ? h * 0.75 : h * 0.8,
-                  child: SingleChildScrollView(
-                    child: _getController.widgetOptions.elementAt(_getController.index.value),
+                SizedBox(width: w * 0.04),
+                Image(
+                  image: const AssetImage('assets/images/text.png'),
+                  width: w * 0.2,
+                  height: h * 0.05,
+                  color: _getController.index.value == 1
+                      ? Colors.white
+                      : Colors.blue,
+                ),
+                Expanded(child: Container()),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MessagingWidget()),
+                    );
+                  },
+                  child: HeroIcon(
+                    HeroIcons.bell,
+                    size: w * 0.07,
+                    color: _getController.index.value == 1
+                        ? Colors.white
+                        : Colors.blue,
                   ),
                 ),
+                SizedBox(width: w * 0.02),
               ],
-            )
+            ),
+          ) : Container(height: h * 0.03))),
+
+      body: Obx(() => _getController.index.value == 3 || _getController.index.value == 4 || _getController.index.value == 1
+          ? SizedBox(
+        child: _getController.widgetOptions.elementAt(_getController.index.value),
+      ) : Column(
+        children: [
+          Obx(() => _getController.uplAodVideo.value == true
+              ? Container(
+              width: w,
+              height: h * 0.05,
+              color: Colors.grey[300],
+              padding: EdgeInsets.only(left: w * 0.02, right: w * 0.02),
+              child: Row(
+                children: [
+                  _getController.postFile.value != ''
+                      ? Container(
+                    width: w * 0.1,
+                    height: h * 0.04,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(3),
+                      image: DecorationImage(
+                        image: Image.file(File(_getController.postFile.value),).image,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                      : Container(),
+                  SizedBox(width: w * 0.02),
+                  Text('Yuklanmoqda...',
+                    style: TextStyle(
+                      fontSize: w * 0.04,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  const SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: CircularProgressIndicator(),
+                  )
+                ],
+              ))
+              : const SizedBox()),
+          SizedBox(
+            height: _getController.uplAodVideo.value == true ? h * 0.75 : h * 0.831,
+            child: SingleChildScrollView(
+              child: _getController.widgetOptions.elementAt(_getController.index.value),
+            ),
+          ),
+        ],
+      )
       ),
       bottomNavigationBar: Obx(
-        () => BottomNavigationBar(
+            () => BottomNavigationBar(
           backgroundColor: Colors.white,
           unselectedItemColor: Colors.black,
           showSelectedLabels: false,
@@ -192,7 +202,7 @@ class SamplePage extends StatelessWidget {
           currentIndex: _getController.index.value,
           onTap: _onItemTapped,
         ),
-      ),
+      ),)
     );
   }
 }
