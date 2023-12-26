a81d4390d7e483b889c133392ae76f10276443c3
to-rex <torex.amaki@gmail.com>
2023-12-26 22:02:54 +0500

    first

--- lib/pages/professions_list_elements.dart
+++ lib/pages/professions_list_elements.dart
@@ -17,6 +17,7 @@ class ProfessionsListElements extends StatelessWidget {
   }) : super(key: key);
   final GetController _getController = Get.put(GetController());
   final RefreshController _refreshController = RefreshController(initialRefresh: false);
+  var scrollController = ScrollController();
 
   void _onRefresh() async {
     ApiController().getSubCategory(index!.toInt()).then((value) => {
@@ -206,7 +207,7 @@ class ProfessionsListElements extends StatelessWidget {
             centerTitle: true),
           ),
           Positioned(
-              top: h * 0.05,
+              top: h * 0.04,
               child: SizedBox(
                 height: h * 0.2,
                 width: w,
@@ -220,28 +221,75 @@ class ProfessionsListElements extends StatelessWidget {
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(6),
                       ),
-                      child: Center(
-                        child: HeroIcon(
-                          HeroIcons.chevronLeft,
-                          color: Colors.blue,
-                          size: w * 0.05,
+                      child: InkWell(
+                        onTap: () {
+                          scrollController.animateTo(
+                            scrollController.offset - w * 0.3,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                        child: Center(
+                          child: HeroIcon(
+                            HeroIcons.chevronLeft,
+                            color: Colors.blue,
+                            size: w * 0.05,
+                          ),
                         ),
                       ),
                     ),
-                    Expanded(child: SizedBox()),
+                    Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(left: w * 0.05),
+                          child: Obx(() => _getController.getFollowPost.value.res!.isNotEmpty
+                              ? SizedBox(
+                            height: h * 0.11,
+                            child: ListView(
+                              controller: scrollController,
+                              scrollDirection: Axis.horizontal,
+                              semanticChildCount: _getController.getFollowPost.value.res!.length,
+                              children: [
+                                for (int i = 0; i < _getController.getFollowPost.value.res!.length; i++)
+                                  Container(
+                                    height: h * 0.03,
+                                    width: w * 0.3,
+                                    margin: EdgeInsets.only(right: w * 0.02),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(6),
+                                      image: DecorationImage(
+                                        image: NetworkImage('${_getController.getFollowPost.value.res?[i].photo}'),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                              ],),
+                          ) : const SizedBox()
+                          ),
+                        )
+                    ),
                     Container(
-                      margin: EdgeInsets.only(right: w * 0.05),
+                      margin: EdgeInsets.only(right: w * 0.05, left: w * 0.05),
                       height: h * 0.03,
                       width: w * 0.06,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(6),
                       ),
-                      child: Center(
-                        child: HeroIcon(
-                          HeroIcons.chevronRight,
-                          color: Colors.blue,
-                          size: w * 0.05,
+                      child: InkWell(
+                        onTap: () {
+                          scrollController.animateTo(
+                            scrollController.offset + w * 0.3,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                        child: Center(
+                          child: HeroIcon(
+                            HeroIcons.chevronRight,
+                            color: Colors.blue,
+                            size: w * 0.05,
+                          ),
                         ),
                       ),
                     ),
@@ -250,126 +298,145 @@ class ProfessionsListElements extends StatelessWidget {
               ),
             ),
           Positioned(
-              top: h * 0.25,
+              top: h * 0.23,
               child: Container(
+                width: w,
                 color: Colors.white,
-                child: Obx(() => _getController.subCategory.value.res == null
-                    ? Center(child: Text('Ma`lumotlar yo`q', style: TextStyle(fontSize: w * 0.04)))
-                    : Container(
-                  margin: EdgeInsets.only(top: h * 0.02),
+                child: Obx(() => _getController.subCategory.value.res == null || _getController.subCategory.value.res!.isEmpty
+                    ? SizedBox(
                   height: h * 0.755,
                   width: w,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Expanded(child: SizedBox()),
+                      Text('Ma`lumotlar yo`q', style: TextStyle(fontSize: w * 0.04)),
+                      SizedBox(height: h * 0.2),
+                      const Expanded(child: SizedBox()),
+                    ],
+                  ),
+                )
+                    : Container(
+                  height: h * 0.6,
+                  width: w * 0.9,
+                  margin: EdgeInsets.only(top: h * 0.02),
+                  padding: EdgeInsets.only(bottom: h * 0.03),
                   child: SmartRefresher(
-                      enablePullDown: true,
-                      enablePullUp: true,
-                      header: CustomHeader(
-                        builder: (BuildContext context, RefreshStatus? mode) {
-                          Widget body;
-                          if (mode == RefreshStatus.idle) {
-                            body = const Text("Ma`lumotlarni yangilash uchun tashlang");
-                          } else if (mode == RefreshStatus.refreshing) {
-                            body = const CircularProgressIndicator(
-                              color: Colors.blue,
-                              backgroundColor: Colors.white,
-                              strokeWidth: 2,
-                            );
-                          } else if (mode == RefreshStatus.failed) {
-                            body = const Text("Ex Nimadir Xato ketdi");
-                          } else if (mode == RefreshStatus.canRefresh) {
-                            body = const Text("Ma`lumotlarni yangilash uchun tashlang");
-                          } else {
-                            body = const Text("Ma`lumotlar yangilandi");
-                          }
-                          return SizedBox(
-                            height: h * 0.1,
-                            child: Center(child: body),
+                    enablePullDown: true,
+                    enablePullUp: true,
+                    header: CustomHeader(
+                      builder: (BuildContext context, RefreshStatus? mode) {
+                        Widget body;
+                        if (mode == RefreshStatus.idle) {
+                          body = const Text("Ma`lumotlarni yangilash uchun tashlang");
+                        } else if (mode == RefreshStatus.refreshing) {
+                          body = const CircularProgressIndicator(
+                            color: Colors.blue,
+                            backgroundColor: Colors.white,
+                            strokeWidth: 2,
                           );
-                        },
-                      ),
-                      footer: CustomFooter(
-                        builder: (BuildContext context, LoadStatus? mode) {
-                          Widget body;
-                          if (mode == LoadStatus.idle) {
-                            body = const SizedBox();
-                          } else if (mode == LoadStatus.loading) {
-                            body = const CircularProgressIndicator(
-                              color: Colors.blue,
-                              backgroundColor: Colors.white,
-                              strokeWidth: 2,
-                            );
-                          } else if (mode == LoadStatus.failed) {
-                            body = const Text("Ex Nimadir Xato ketdi");
-                          } else if (mode == LoadStatus.canLoading) {
-                            body = const SizedBox();
-                          } else {
-                            body = const Text("Ma`lumotlar yangilandi");
-                          }
-                          return SizedBox(
-                            height: h * 0.1,
-                            child: Center(child: body),
+                        } else if (mode == RefreshStatus.failed) {
+                          body = const Text("Load Failed!Click retry!");
+                        } else if (mode == RefreshStatus.canRefresh) {
+                          body = const Text("Ma`lumotlarni yangilash uchun tashlang");
+                        } else {
+                          body = const Text("Ma`lumotlar yangilandi");
+                        }
+                        return SizedBox(
+                          height: h * 0.1,
+                          child: Center(child: body),
+                        );
+                      },
+                    ),
+                    footer: CustomFooter(
+                      builder: (BuildContext context, LoadStatus? mode) {
+                        Widget body;
+                        if (mode == LoadStatus.idle) {
+                          body = const SizedBox();
+                        } else if (mode == LoadStatus.loading) {
+                          body = const CircularProgressIndicator(
+                            color: Colors.blue,
+                            backgroundColor: Colors.white,
+                            strokeWidth: 2,
                           );
-                        },
-                      ),
-                      controller: _refreshController,
-                      onRefresh: _onRefresh,
-                      onLoading: _onLoading,
-                      child: ListView.builder(
-                        itemBuilder: (context , index) {
-                          return InkWell(
-                            splashColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            focusColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            onTap: () {
-                              _getController.categoryByID.value = _getController.subCategory.value.res![index].id!;
-                              _getController.enters.value = 2;
-                              _getController.changeTitleListElements(_getController.subCategory.value.res![index].name!);
-                            },
-                            child: Container(
-                                height: h * 0.04,
-                                width: w * 0.98,
-                                margin: EdgeInsets.only(bottom: h * 0.02),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        SizedBox(width: w * 0.02),
-                                        Text(_getController.subCategory.value.res?[index].name ?? '',
-                                          style: TextStyle(
-                                            fontSize: w * 0.04,
-                                            fontWeight: FontWeight.w500,
-                                          ),
-                                        ),
-                                        const Expanded(child: SizedBox()),
-                                        Icon(Icons.arrow_forward_ios, size: w * 0.04),
-                                        SizedBox(width: w * 0.02),
-                                      ],
-                                    ),
-                                    SizedBox(height: h * 0.01),
-                                    Divider(
-                                      color: Colors.grey[300],
-                                      thickness: 1,
-                                      height: 1,
-                                    ),
-                                  ],
-                                )
+                        } else if (mode == LoadStatus.failed) {
+                          body = const Text("Load Failed!Click retry!");
+                        } else if (mode == LoadStatus.canLoading) {
+                          body = const SizedBox();
+                        } else {
+                          body = const Text("Ma`lumotlar yangilandi");
+                        }
+                        return SizedBox(
+                          height: h * 0.1,
+                          child: Center(child: body),
+                        );
+                      },
+                    ),
+                    controller: _refreshController,
+                    onRefresh: _onRefresh,
+                    onLoading: _onLoading,
+                    child: ListView.builder(
+                      itemCount: _getController.subCategory.value.res?.length,
+                      padding: EdgeInsets.only(left: w * 0.05, right: w * 0.05),
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            _getController.categoryByID.value = _getController.subCategory.value.res![index].id!;
+                            _getController.enters.value = 2;
+                            _getController.changeTitleListElements(_getController.subCategory.value.res![index].name!);
+                          },
+                          child: Container(
+                            height: h * 0.06,
+                            margin: EdgeInsets.only(bottom: h * 0.02),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(w * 0.02),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 1),
+                                ),
+                              ],
                             ),
-                          );
-                        },
-                        itemCount: _getController.subCategory.value.res?.length,
-                        cacheExtent: w * 0.1,
-                        dragStartBehavior: DragStartBehavior.down,
-                        prototypeItem: Container(
-                          height: h * 0.04,
-                          margin: EdgeInsets.only(bottom: h * 0.02),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(w * 0.02),
+                            child: Row(
+                              children: [
+                                SizedBox(width: w * 0.04),
+                                SizedBox(
+                                  width: w * 0.6,
+                                  child: Text(
+                                    _getController.subCategory.value.res?[index].name ?? '',
+                                    maxLines: 1,
+                                    style: TextStyle(
+                                      fontSize: w * 0.04,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                                const Expanded(child: SizedBox()),
+                                Container(
+                                  width: w * 0.08,
+                                  height: h * 0.04,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: Colors.blue,
+                                      size: w * 0.05,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: w * 0.04),
+                              ],
+                            ),
                           ),
-                        ),
-                      )),
+                        );
+                      },
+                    ),
+                  ),
                 ),
                 ),
               )
